ce3ffbd3f14a07843d1b7de54c9e514fa3f9cd56
211_0@naver.com <211_0@naver.com>
2023-11-04 17:00:29 +0900

    rating 정보를 가져오기 위한 get 코드

---
+++ lib/get_rating.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+Future<void> fetchUserInfo(String id) async {
+  var url = Uri.parse('API_URL'); // 사용자 정보를 가져올 API의 URL
+
+  var response = await http.get(
+    url,
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+      'Authorization': 'Bearer $id', // 아이디를 포함하여 요청하기 (사용자 식별용)
+    },
+  );
+
+  if (response.statusCode == 200) {
+    // userData: rating
+    var userData = jsonDecode(response.body);
+    print('$userData');
+  } else {
+    print('Failed');
+  }
+}
