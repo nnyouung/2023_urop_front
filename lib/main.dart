169b69b942ff0dc220d8ec1c2362e262422ff7a8
meoowwing <eojin28@naver.com>
2023-11-15 12:15:44 +0900

    로그인, 회원가입 서버 통신 코드 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:shared_preferences/shared_preferences.dart'; // 토큰 저장을 위한 패키지
+import 'package:shared_preferences/shared_preferences.dart';
 import 'home.dart';
 import 'signup.dart';
 
 void main() {
-  runApp(const MyLogin());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sudoku',
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+      ),
+      home: const MyLogin(), // Home 또는 원하는 위젯으로 대체
+      routes: const {
+        // 여기에 라우트 추가
+      },
+    );
+  }
 }
 
 class MyLogin extends StatelessWidget {
@@ -19,7 +37,7 @@ class MyLogin extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.grey,
       ),
-      home: const LoginPage(title: 'Login'),
+      home: const LoginPage(title: 'LOGIN PAGE'),
       initialRoute: '/',
       routes: {
         '/home': (context) => const MyHomePage(title: 'Home'),
@@ -38,11 +56,53 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
 
-  // 서버 주소
-  final String serverUrl = 'http://localhost:'; // Django 서버 주소로 변경해야 함
+  // 로그인 함수
+  Future<void> login() async {
+    String email = emailController.text;
+    String password = passwordController.text;
+
+    try {
+      // 서버로 로그인 요청
+      final response = await http.post(
+        Uri.parse('http://ec2-54-172-150-42.compute-1.amazonaws.com:8080/users/login'),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode({
+          'email': email,
+          'password': password,
+        }),
+      );
+
+      // 서버 응답이 성공(상태코드 200)이면
+      if (response.statusCode == 200) {
+        // 서버 응답 데이터에서 사용자 ID 추출
+        final Map<String, dynamic> responseData = jsonDecode(response.body);
+        final String userId = responseData['id'].toString();
+
+        // 사용자 ID를 SharedPreferences를 사용해 저장
+        final SharedPreferences prefs = await SharedPreferences.getInstance();
+        prefs.setString('userId', userId);
+
+        // 홈 페이지로 이동
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => MyHomePage(title: 'Home'),
+          ),
+        );
+      } else {
+        // 로그인 실패 시 에러 메시지 출력
+        print('로그인 실패, statusCode: ${response.statusCode}');
+      }
+    } catch (error) {
+      // 예외 발생 시 에러 메시지 출력
+      print('에러 발생: $error');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -54,29 +114,32 @@ class _LoginPageState extends State<LoginPage> {
         ),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0), // 여백 설정
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            // 아이디 입력 필드
+            // 이메일 입력 필드
             TextField(
-              controller: usernameController, // 컨트롤러 설정
+              controller: emailController,
               decoration: const InputDecoration(
-                labelText: '아이디 입력', // 레이블 텍스트 설정
+                labelText: '이메일 입력',
               ),
             ),
             // 비밀번호 입력 필드
             TextField(
-              controller: passwordController, // 컨트롤러 설정
-              obscureText: true, // 비밀번호 입력 시 입력 내용을 가려서 보이지 않게 함
+              controller: passwordController,
+              obscureText: true,
               decoration: const InputDecoration(
-                labelText: '비밀번호 입력', // 레이블 텍스트 설정
+                labelText: '비밀번호 입력',
               ),
             ),
             const SizedBox(height: 20),
+            // 회원가입 및 로그인 버튼
             Row(
+              mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 ElevatedButton(
                   onPressed: () {
+                    // 회원가입 페이지로 이동
                     Navigator.push(
                       context,
                       MaterialPageRoute(
@@ -92,9 +155,9 @@ class _LoginPageState extends State<LoginPage> {
                     style: TextStyle(color: Colors.white),
                   ),
                 ),
-                const SizedBox(width: 20), // 간격 조정
+                const SizedBox(width: 20),
                 ElevatedButton(
-                  onPressed: () => login(context),
+                  onPressed: () => login(),
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.black,
                   ),
@@ -108,57 +171,6 @@ class _LoginPageState extends State<LoginPage> {
           ],
         ),
       ),
-    );
-  }
-
-  Future<void> login(BuildContext context) async {
-    String username = usernameController.text;
-    String password = passwordController.text;
-
-    if (username == "admin" && password == "password") {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(
-          builder: (context) =>
-              const MyHomePage(title: 'Home'), // 로그인 성공 시 이동할 페이지
-        ),
-      );
-    } else {
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: const Text("로그인 실패"),
-            content: const Text("아이디나 비밀번호가 올바르지 않습니다."),
-            actions: [
-              TextButton(
-                child: const Text("확인"),
-                onPressed: () {
-                  Navigator.of(context).pop(); // 다이얼로그 닫기
-                },
-              ),
-            ],
-          );
-        },
-      );
-    }
-  }
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Sudoku',
-      theme: ThemeData(
-        primarySwatch: Colors.grey,
-      ),
-      home: const MyLogin(), // Home 또는 원하는 위젯으로 대체
-      routes: const {
-        // 여기에 라우트 추가
-      },
     );
   }
 }
--- lib/signup.dart
+++ lib/signup.dart
@@ -3,98 +3,60 @@ import 'package:http/http.dart' as http; // HTTP 패키지 import
 import 'dart:convert'; // JSON 인코딩/디코딩을 위한 패키지 import
 
 class SignupPage extends StatelessWidget {
-  final TextEditingController idController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   final TextEditingController passwordCheckController = TextEditingController();
 
   SignupPage({super.key});
 
-  Future<void> check(BuildContext context) async {
-    // 각각의 입력 필드에서 사용자가 입력한 값을 가져옴
-    String id = idController.text;
+  Future<void> signup() async {
+    String email = emailController.text;
     String password = passwordController.text;
     String passwordCheck = passwordCheckController.text;
 
-    // 필수 필드가 모두 채워져 있는지 확인
-    if (id.isEmpty || password.isEmpty || passwordCheck.isEmpty) {
-      // 필수 필드 중 하나라도 비어 있으면 경고 다이얼로그 표시
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: const Text("경고"),
-            content: const Text("모든 필드를 입력해주세요."),
-            actions: [
-              TextButton(
-                child: const Text("확인"),
-                onPressed: () {
-                  Navigator.of(context).pop(); // 다이얼로그 닫기
-                },
-              ),
-            ],
-          );
+    try {
+      final response = await http.post(
+        Uri.parse('http://ec2-54-172-150-42.compute-1.amazonaws.com:8080/users/signup'),
+        headers: {
+          'Content-Type': 'application/json',
         },
+        body: jsonEncode({
+          'email': email,
+          'password': password,
+          'passwordCheck': passwordCheck,
+        }),
       );
-      return;
-    }
 
-    // 서버로 회원가입 요청을 보낼 URL 설정 (Django 서버의 URL에 맞게 변경해야 함)
-    String signupUrl = 'http://ec2-54-172-150-42.compute-1.amazonaws.com/signup';
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        print(data);
 
-    // POST 요청 데이터 생성
-    Map<String, String> requestData = {
-      'id': id,
-      'password': password,
-      'passwordCheck': passwordCheck,
-    };
-
-    // POST 요청 보내기
-    final response = await http.post(
-      Uri.parse(signupUrl),
-      body: requestData,
-    );
-
-    if (response.statusCode == 200) {
-      // 회원가입 성공 시의 동작
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: const Text("회원가입 성공"),
-            content: const Text("회원가입이 완료되었습니다."),
-            actions: [
-              TextButton(
-                child: const Text("확인"),
-                onPressed: () {
-                  Navigator.of(context).pop(); // 다이얼로그 닫기
-                  Navigator.pop(context); // 회원가입 성공 시 메인 화면으로 이동
-                },
-              ),
-            ],
-          );
-        },
-      );
-    } else {
-      // 회원가입 실패 시의 동작
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: const Text("회원가입 실패"),
-            content: const Text("회원가입에 실패하였습니다."),
-            actions: [
-              TextButton(
-                child: const Text("확인"),
-                onPressed: () {
-                  Navigator.of(context).pop(); // 다이얼로그 닫기
-                },
-              ),
-            ],
-          );
-        },
-      );
+        // 회원가입 성공 시의 동작 (여기서는 간단히 출력만)
+        print('회원가입 성공');
+      } else {
+        // 회원가입 실패 시의 동작 (여기서는 간단히 출력만)
+        print('회원가입 실패, statusCode: ${response.statusCode}');
+      }
+    } catch (error) {
+      // 예외 처리 (네트워크 에러 등)
+      print('에러 발생: $error');
     }
   }
+
+  // Widget signupButton() {
+  //   return ElevatedButton(
+  //     onPressed: () async {
+  //       await check();
+  //     },
+  //     style: ElevatedButton.styleFrom(
+  //       padding: EdgeInsets.all(0.0),
+  //     ),
+  //     child: const Text(
+  //       '회원가입',
+  //       style: TextStyle(fontSize: 9),
+  //     ),
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -106,42 +68,40 @@ class SignupPage extends StatelessWidget {
         ),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0), // 여백 설정
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            // 아이디 입력 필드
+            // 이메일 입력 필드
             TextField(
-              controller: idController, // 컨트롤러 설정
+              controller: emailController,
               decoration: const InputDecoration(
-                labelText: 'ID', // 레이블 텍스트 설정
+                labelText: 'EMAIL',
               ),
             ),
-            // 비밀번호 입력 필드
             TextField(
               controller: passwordController,
-              obscureText: true, // 비밀번호 입력 시 입력 내용을 가려서 보이지 않게 함
+              obscureText: true,
               decoration: const InputDecoration(
-                labelText: 'PASSWORD',
+                labelText: 'PASSWORD (8자 이상 입력하시오.)',
               ),
             ),
-            // 비밀번호 확인 필드
             TextField(
               controller: passwordCheckController,
-              obscureText: true, // 비밀번호 확인 시 입력 내용을 가려서 보이지 않게 함
+              obscureText: true,
               decoration: const InputDecoration(
                 labelText: 'CHECK PASSWORD',
               ),
             ),
-            const SizedBox(height: 20), // 위젯간의 간격 조정
+            const SizedBox(height: 20),
             Align(
               alignment: Alignment.centerRight,
               child: ElevatedButton(
-                onPressed: () => check(context), // 버튼 텍스트 스타일 설정
+                onPressed: () => signup(), // 버튼 텍스트 스타일 설정
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.black, // 버튼 색상 설정
                 ), // 버튼 클릭 시 check 함수 호출
                 child: const Text(
-                  'SIGNUP',
+                  '회원가입',
                   style: TextStyle(color: Colors.white),
                 ),
               ),
