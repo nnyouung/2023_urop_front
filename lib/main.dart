e1aee2d053aafad1337daea556531d8a8b3883d1
meoowwing <eojin28@naver.com>
2023-11-19 15:21:51 +0900

    로그인, 회원가입 팝업창 및 주석 추가

--- lib/main.dart
+++ lib/main.dart
@@ -59,11 +59,43 @@ class _LoginPageState extends State<LoginPage> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
 
-  // 로그인 함수
-  Future<void> login() async {
+  // 로그인 결과를 팝업으로 표시하는 함수
+  Future<void> showResultDialog(BuildContext context, String message, bool success) async {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('로그인 결과'),
+          content: Text(message), // 로그인 결과 메시지 표시
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // 팝업 창 닫기
+
+                if (success) {
+                  // 로그인이 성공하면 홈 화면으로 이동
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => MyHomePage(title: 'Home'),
+                    ),
+                  );
+                } else {
+                  // 로그인 실패 또는 오류가 있으면 로그인 화면에 남아 있도록 함
+                }
+              },
+              child: Text('확인'), // 확인 버튼 텍스트
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  // 로그인을 시도하는 함수
+  Future<void> login(BuildContext context) async {
     String email = emailController.text;
     String password = passwordController.text;
-    print(email);
 
     try {
       // 서버로 로그인 요청
@@ -78,30 +110,31 @@ class _LoginPageState extends State<LoginPage> {
         }),
       );
 
-      // 서버 응답이 성공(상태코드 200)이면
+      // 서버 응답 확인
       if (response.statusCode == 200) {
-        // 서버 응답 데이터에서 사용자 ID 추출
+        // 서버 응답 데이터에서 사용자 이메일 추출
         final Map<String, dynamic> responseData = jsonDecode(response.body);
         final String email = responseData['email'].toString();
 
-        // 사용자 ID를 SharedPreferences를 사용해 저장
+        // 사용자 이메일을 SharedPreferences를 사용해 저장
         final SharedPreferences prefs = await SharedPreferences.getInstance();
         prefs.setString('email', email);
 
-        // 홈 페이지로 이동
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(
-            builder: (context) => MyHomePage(title: 'Home'),
-          ),
-        );
+        // 로그인 성공 시 성공 메시지 출력
+        print('로그인 성공');
+        // 로그인 성공 시 팝업창 표시
+        showResultDialog(context, '로그인 성공', true);
       } else {
         // 로그인 실패 시 에러 메시지 출력
         print('로그인 실패, statusCode: ${response.statusCode}');
+        // 로그인 실패 시 팝업창 표시
+        showResultDialog(context, '로그인 실패', false);
       }
-    } catch (error) {
+    } catch (error) { // 예외 처리 (네트워크 에러 등)
       // 예외 발생 시 에러 메시지 출력
       print('에러 발생: $error');
+      // 예외 발생 시 팝업창 표시
+      showResultDialog(context, '에러 발생: $error', false);
     }
   }
 
@@ -157,11 +190,12 @@ class _LoginPageState extends State<LoginPage> {
                   ),
                 ),
                 const SizedBox(width: 20),
+                // 로그인 버튼
                 ElevatedButton(
-                  onPressed: () => login(),
+                  onPressed: () => login(context), // context를 전달하여 팝업창이 표시될 수 있도록 함
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.black,
-                  ),
+                  ), // 버트 클릭 시 login 함수 호출
                   child: const Text(
                     '로그인',
                     style: TextStyle(color: Colors.white),
--- lib/signup.dart
+++ lib/signup.dart
@@ -9,13 +9,35 @@ class SignupPage extends StatelessWidget {
 
   SignupPage({super.key});
 
-  // 회원가입 함수
-  Future<void> signup() async {
+  // 회원가입 결과를 팝업으로 표시하는 함수
+  Future<void> showResultDialog(BuildContext context, String message) async {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('회원가입 결과'),
+          content: Text(message), // 회원가입 결과 메시지 표시
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // 팝업 창 닫기
+              },
+              child: Text('확인'), // 확인 버튼 텍스트
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  // 회원가입을 시도하는 함수
+  Future<void> signup(BuildContext context) async {
     String email = emailController.text;
     String password = passwordController.text;
     String passwordCheck = passwordCheckController.text;
 
     try {
+      // 서버에 회원가입 요청
       final response = await http.post(
         Uri.parse('http://ec2-54-172-150-42.compute-1.amazonaws.com:8080/users/signup'),
         headers: {
@@ -28,19 +50,26 @@ class SignupPage extends StatelessWidget {
         }),
       );
 
+      // 서버 응답 확인
       if (response.statusCode == 200) {
         final data = jsonDecode(response.body);
         print(data);
 
-        // 회원가입 성공 시의 동작 (여기서는 간단히 출력만)
+        // 회원가입 성공 시 성공 메시지 출력
         print('회원가입 성공');
+        // 회원가입 성공 시 팝업창 표시
+        showResultDialog(context, '회원가입 성공');
       } else {
-        // 회원가입 실패 시의 동작 (여기서는 간단히 출력만)
+        // 회원가입 실패 시 에러 메시지 출력
         print('회원가입 실패, statusCode: ${response.statusCode}');
+        // 회원가입 실패 시 팝업창 표시
+        showResultDialog(context, '회원가입 실패');
       }
-    } catch (error) {
-      // 예외 처리 (네트워크 에러 등)
+    } catch (error) { // 예외 처리 (네트워크 에러 등)
+      // 예외 발생 시 에러 메시지 출력
       print('에러 발생: $error');
+      // 예외 발생 시 팝업창 표시
+      showResultDialog(context, '에러 발생: $error');
     }
   }
 
@@ -64,6 +93,7 @@ class SignupPage extends StatelessWidget {
                 labelText: 'EMAIL',
               ),
             ),
+            // 비밀번호 입력 필드
             TextField(
               controller: passwordController,
               obscureText: true,
@@ -71,6 +101,7 @@ class SignupPage extends StatelessWidget {
                 labelText: 'PASSWORD (8자 이상 입력하시오.)',
               ),
             ),
+            // 비밀번호 확인 입력 필드
             TextField(
               controller: passwordCheckController,
               obscureText: true,
@@ -79,13 +110,14 @@ class SignupPage extends StatelessWidget {
               ),
             ),
             const SizedBox(height: 20),
+            // 회원가입 버튼
             Align(
               alignment: Alignment.centerRight,
               child: ElevatedButton(
-                onPressed: () => signup(), // 버튼 텍스트 스타일 설정
+                onPressed: () => signup(context), // context를 전달하여 팝업창이 표시될 수 있도록 함
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.black, // 버튼 색상 설정
-                ), // 버튼 클릭 시 check 함수 호출
+                ), // 버튼 클릭 시 signup 함수 호출
                 child: const Text(
                   '회원가입',
                   style: TextStyle(color: Colors.white),
